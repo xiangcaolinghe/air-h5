ddbde625a29aee570fbafdd7eb3c6f2b34549662
徐茹 <xuru@dino-tech.com>
2018-09-19 16:32:49 +0800

    登陆页面 完成

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import App from './App'
 import router from './router'
 // import API from './config/API';
 import API from './config/APIHeader';
-// import store from './store/store';
+import LocalStorageUtils from  './utils/LocalStorageUtils'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 window.API = API;
 Vue.config.productionTip = false
+const storage = new LocalStorageUtils();
+window.storage = storage;
 new Vue({
   el: '#app',
   router,
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,19 +7,41 @@
                         <img src="./../../assets/logo.png" alt="" class="img">
                     </router-link>
                     <!--登陆-->
-                    <div class="input-table">
+                    <div class="input-table" v-show="show">
                         <div class="cell user">
                             <span class="name">账号</span>
-                            <input type="text" class="input" placeholder="请输入账号/手机号">
+                            <input type="text" class="input" placeholder="请输入账号/手机号" v-model="userNum">
                         </div>
                         <div class="cell password">
                             <span class="name">密码</span>
-                            <input type="text" class="input" placeholder="请输入密码">
+                            <input type="text" class="input" placeholder="请输入密码" v-model="userPassword">
                         </div>
-                        <button class="login-btn">登陆</button>
-                        <div class="tip">没有账号？ <span class="link">立即注册</span></div>
+                        <button class="login-btn" @click="login">登陆</button>
+                        <div class="tip">没有账号？ <span class="link" @click="showRegister">立即注册</span></div>
                     </div>
                     <!--注册-->
+                    <div class="input-table register-table" v-show="!show">
+                        <div class="cell user">
+                            <span class="name">用户名</span>
+                            <input type="text" class="input" placeholder="请填写用户名称" v-model="name">
+                        </div>
+                        <div class="cell">
+                            <span class="name">手机号</span>
+                            <input type="text" class="input" placeholder="请输入手机号" v-model="mobile">
+                        </div>
+                        <div class="cell code">
+                            <span class="name">验证码</span>
+                            <div class="code-btn" @click="getCode">获取验证码</div>
+                            <input type="text" class="input" placeholder="请输入手机验证码" v-model="code">
+                        </div>
+                        <div class="cell">
+                            <span class="name">密<span class="opacity">码</span>码</span>
+                            <input type="text" class="input" placeholder="请输入密码" v-model="password">
+                        </div>
+                        <button class="login-btn" @click="register">注册</button>
+                        <div class="tip">已有账号！ <span class="link" @click="showRegister">立即登陆</span></div>
+                    </div>
+                    <div class="copyright">中国民用航空局空中交通管理局空域管理中心 &copy;版权所有</div>
                 </div>
             </div>
         </div>
@@ -30,11 +52,60 @@
   export default {
     data () {
       return {
-
+        show:true,
+        userNum:'',
+        userPassword:'',
+        name:'',
+        mobile:'',
+        code:'',
+        password:''
       }
     },
     methods:{
-
+      showRegister() {
+        this.show = !this.show
+      },
+      // 注册
+      register() {
+        if (!this.name) {
+          this.$message('请填写用户名');
+          return
+        }
+        if (!this.mobile) {
+          this.$message('请填写电话号码');
+          return
+        }
+        if (!this.code) {
+          this.$message('请填写验证码');
+          return
+        }
+        if (!this.password) {
+          this.$message('请输入密码');
+          return
+        }
+        storage.set('token','3333333333333');
+        this.$router.push({name:'home'})
+      },
+      //登陆
+      login() {
+        if (!this.userNum) {
+          this.$message('请填写用户名');
+          return
+        }
+        if (!this.userPassword) {
+          this.$message('请输入密码');
+          return
+        }
+        storage.set('token','3333333333333');
+        this.$router.push({name:'home'})
+      },
+      // 获取验证码
+      getCode() {
+        if (!this.mobile) {
+          this.$message('请填写电话号码');
+          return
+        }
+      }
     },
     created() {
 
@@ -81,9 +152,6 @@
                         text-align: left;
                         color:#000;
                         line-height: 35px;
-                        .name {
-
-                        }
                         .input {
                             font-size: 14px;
                             height:35px;
@@ -118,6 +186,43 @@
                             cursor: pointer;
                         }
                     }
+                    &.register-table {
+                        height:446px;
+                        .cell {
+                            margin: 0 0 20px;
+                            .opacity {
+                                opacity:0;
+                                filter:alpha(opacity=0);
+                            }
+                            .input {
+                                width:340px;
+                            }
+                            &.code {
+                                .input {
+                                    width:180px;
+                                }
+                                .code-btn {
+                                    float: right;
+                                    width:130px;
+                                    line-height: 35px;
+                                    height:35px;
+                                    color:#fff;
+                                    font-size: 14px;
+                                    cursor: pointer;
+                                    text-align: center;
+                                    margin-left:30px;
+                                    background: #026ab3;
+                                }
+                             }
+                        }
+                     }
+                }
+                .copyright {
+                    text-align: center;
+                    color:#fff;
+                    margin-top: 98px;
+                    letter-spacing:1px;
+                    font-size: 18px;
                 }
             }
         }
