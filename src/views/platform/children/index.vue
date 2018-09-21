100af361f42b5c6c1942e9f64c474f773aa99cbf
徐茹 <xuru@dino-tech.com>
2018-09-21 16:33:11 +0800

    icon 首页

--- src/views/platform/children/index.vue
+++ src/views/platform/children/index.vue
@@ -20,6 +20,15 @@
                 <div class="step">4.核对并上发批复</div>
             </li>
         </ul>
+        <div class="notice">
+            <div class="title">通知公告</div>
+            <div class="n-lists">
+                <div class="n-list">
+                    <i class="icon iconfont icon-ep--"></i>
+                    2018年5月新增班机航线批复
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -53,8 +62,74 @@
         .progress-table {
             width:900px;
             height:3px;
+            position: relative;
             background: #003b64;
             margin:54px auto 160px;
+            .cell {
+                width:25%;
+                top:-15px;
+                position: absolute;
+                .icon {
+                    font-size: 30px;
+                    padding: 0 10px;
+                    background: #f8f8f8;
+                    color:#003b64;
+                }
+                .step {
+                    margin: 46px auto 0;
+                    background: #003b64;
+                    color:#fff;
+                    line-height: 40px;
+                    text-align: center;
+                    height:40px;
+                    width:110px;
+                    font-size: 16px;
+                }
+                &:nth-child(1) {
+                    left:0;
+                    transform: translateX(-50%);
+                 }
+                &:nth-child(2) {
+                     left:33%;
+                     transform: translateX(-50%);
+                 }
+                &:nth-child(3) {
+                     left:66%;
+                     transform: translateX(-50%);
+                     .step {
+                         width:188px;
+                     }
+                 }
+                &:nth-child(4) {
+                     right:0;
+                     transform: translateX(50%);
+                    .step {
+                        width:188px;
+                    }
+                 }
+            }
+        }
+        .notice {
+            .n-lists {
+                width:100%;
+                .n-list {
+                    height:56px;
+                    line-height: 56px;
+                    cursor: pointer;
+                    color:#000;
+                    text-align: left;
+                    border-bottom: 1px solid #ccc;
+                    .icon {
+                        font-size: 22px;
+                        margin:0 30px 0 10px;
+                        color:#666;
+                        font-weight: 500;
+                    }
+                    &:hover {
+                         color:#666;
+                     }
+                }
+            }
         }
     }
 </style>
