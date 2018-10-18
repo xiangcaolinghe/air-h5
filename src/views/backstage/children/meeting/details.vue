d77d0a0956e5886672d4a3232a6c37fc25bdfea9
HYN7656 <40452138+HYN7656@users.noreply.github.com>
2018-10-18 17:36:40 +0800

    会议管理详情页面

--- src/views/backstage/children/meeting/details.vue
+++ src/views/backstage/children/meeting/details.vue
@@ -16,24 +16,76 @@
       </div>
     </div>
     <br/>
-    <div class="news_tails">
-      <div class="news_title">
-        {{datail.title}}
+    <div class="inner-c detail-table">
+      <div class="info">
+        <div class="img"><img class="ewm pic" src="../../../../assets/images/erwm.png"></div>
+        <div class="detail">
+          <div class="title">{{datail.meetingName}}</div>
+          <div class="time"><i class="icon iconfont icon-shijian"></i>{{datail.data[0]}} -- {{datail.data[1]}}</div>
+          <div class="address"><i class="icon iconfont icon-dingweiweizhi"></i>{{datail.meetingPlace}}</div>
+          <div class="btn">在线报名</div>
+          <div class="wechart">
+            <!--<img class="ewm" src="erwm.png">-->
+            <p class="name">扫描二维码报名</p>
+          </div>
+        </div>
       </div>
-      <br/><br/>
-      <div class="dis">
-        <span>作者：{{datail.author}}</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <span>发布时间：{{datail.time}}</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <span>来源：{{datail.origin}}</span>
+      <div class="nav-tabs">
+        <a class="tab" @click="tabActive(1)" :class="{active:active == 1}" href="#detail">会议简介</a>
+        <a class="tab" @click="tabActive(2)" :class="{active:active == 2}" href="#unit">主办单位</a>
+        <a class="tab" @click="tabActive(3)" :class="{active:active == 3}" href="#join">参会相关单位</a>
+        <a class="tab" @click="tabActive(4)" :class="{active:active == 4}" href="#name">会议联系人</a>
+        <a class="tab" @click="tabActive(5)" :class="{active:active == 5}" href="#date">会议日程</a>
       </div>
-      <br/><br/>
-      <div class="detail_content" v-html="datail.content">
+      <div class="nav-content">
+        <!--会议简介-->
+        <a name="detail"></a>
+        <div class="tab">
+          <div class="intro">
+            {{datail.abstract}}
+          </div>
+        </div>
+        <!--主办单位-->
+        <a name="unit"></a>
+        <div class="tab">
+          <div class="title">主办单位</div>
+          <ul>
+            <li v-for="item in datail.partakeHost">{{item}}</li>
+          </ul>
+        </div>
+        <!--参会相关单位-->
+        <a name="join"></a>
+        <div class="tab">
+          <div class="title">参会相关单位</div>
+          <ul>
+            <li v-for="item in datail.partakeCompany">{{item}}</li>
+          </ul>
+        </div>
+        <!--会议联系人-->
+        <a name="name"></a>
+        <div class="tab">
+          <div class="title">会议联系人</div>
+          <ul>
+            <li>姓名 {{datail.contacts}}</li>
+            <li>微信 {{datail.wx}}</li>
+            <li>电话 {{datail.phone}}</li>
+          </ul>
+        </div>
+        <!--会议日程-->
+        <a name="date"></a>
+        <div class="tab">
+          <div class="title">会议日程</div>
+          <div v-html="datail.agenda"></div>
+          <span class="upload">附件点击下载.doc</span>
+        </div>
+        <!--备注-->
+        <div class="tab">
+          <div class="title">备注</div>
+          <ul>
+            <li>请认真填写报名信息，报名信息审核通过后会短信通知详细参会信息。</li>
+          </ul>
+        </div>
       </div>
-      <br/><br/>
-      <div class="down_res">
-        <a href="datail.download_url" style="color: #df6657;text-align: left;">{{datail.download_name}}</a>
-      </div>
-      <br/><br/><br/>
     </div>
   </div>
 </template>
@@ -42,59 +94,38 @@
     export default {
       data(){
         return{
-          datail:{}
+          datail:{
+            meetingName : '',
+            data : [],
+            meetingPlace : '',
+            abstract : '',
+            partakeHost : [],
+            partakeCompany : [],
+            contacts : '',
+            wx : '',
+            phone : '',
+            agenda : '',
+            remarks : ''
+          },
+          active:1,
+          abc : './../../../assets/logo.png'
         }
       },
       mounted: function () {
         this.getDetail();
       },
-      methods:{
-        getDetail(){
-          /*$http.ajax({
-            type: 'post',
-            url: '',
-            data: {},
-            success: function (data) {
-
-            },
-            error: function (error) {
-
+      methods: {
+        getDetail() {
+          let params = {};
+          params['id'] = this.$route.query.id;
+          API.get('static/mettingDetails.json', params).then((res) => {
+            if (res.status == 200) {
+              this.datail = res.data[0];
+              console.log(this.datail)
             }
-          })*/
-          this.datail={
-            title: '公告-西北空管局管制中心开展复盘分析持续改进航班正常工作',
-              author: '空域中心',
-              time: '2018-08-08 12:20',
-              origin: '民航空管局',
-              download_name: '关于更新班机航线密钥证书等事宜的通知.doc',
-              download_url: '/utils/upload.js',
-              content: '<table width="1024" border="0" align="center" cellpadding="0" cellspacing="0">\n' +
-            '        <tr align="center" valign="middle">\n' +
-            '            <td height="196" align="center" valign="top">\n' +
-            '                <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0" >\n' +
-            '                    <tr>\n' +
-            '                        <td align="center" height="10" style="width: 100%" valign="top">\n' +
-            '                        </td>\n' +
-            '                    </tr>\n' +
-            '                    <tr>\n' +
-            '                        <td height="20" style="width: 100%; height: 500px" valign="top">\n' +
-            '                            <table width="85%" border="0" align="center" cellpadding="0" cellspacing="0"\n' +
-            '                                height="200px">\n' +
-            '                                <tr>\n' +
-            '                                    <td valign="top" style="line-height: 200%; width: 850px; height: 200px;" color="Black"\n' +
-            '                                        align="left" class="word">\n' +
-            '                                       \n' +
-            '                                            <span id="Content" TextAlign="middlecenter" style="color:Black;"><p style="TEXT-JUSTIFY: inter-ideograph; TEXT-ALIGN: justify; LINE-HEIGHT: 1.5; MARGIN-TOP: 0px; TEXT-INDENT: 2em; FONT-FAMILY: 宋体; MARGIN-BOTTOM: 0px; FONT-SIZE: 12pt">为了进一步巩固良好的航班正常工作态势，2018年8月2日，西北空管局管制中心组织召开航班正常性复盘分析会议，汲取工作经验，持续改进航班正常工作。</p><p style="TEXT-JUSTIFY: inter-ideograph; TEXT-ALIGN: justify; LINE-HEIGHT: 1.5; MARGIN-TOP: 0px; TEXT-INDENT: 2em; FONT-FAMILY: 宋体; MARGIN-BOTTOM: 0px; FONT-SIZE: 12pt"><br></p><p style="TEXT-JUSTIFY: inter-ideograph; TEXT-ALIGN: justify; LINE-HEIGHT: 1.5; MARGIN-TOP: 0px; TEXT-INDENT: 2em; FONT-FAMILY: 宋体; MARGIN-BOTTOM: 0px; FONT-SIZE: 12pt">2018年上半年以来，西北空管局管制中心主动作为，承接咸阳机场运管委战略解码指标，努力促进航班正常性工作，取得了良好成绩，西安咸阳国际机场放行正常率达到92.21%，始发正常率达92.18%，两项指标均位列全国同级机场第一。同时，咸阳机场离港航班正常率达83.99%，平均滑入时间、平均滑出时间、关舱门平均等待时间较去年同期均有明显缩短，咸阳机场整体运行效率和旅客的出行体验均获得显著提升。</p><p style="TEXT-JUSTIFY: inter-ideograph; TEXT-ALIGN: justify; LINE-HEIGHT: 1.5; MARGIN-TOP: 0px; TEXT-INDENT: 2em; FONT-FAMILY: 宋体; MARGIN-BOTTOM: 0px; FONT-SIZE: 12pt"><br></p><p style="TEXT-JUSTIFY: inter-ideograph; TEXT-ALIGN: justify; LINE-HEIGHT: 1.5; MARGIN-TOP: 0px; TEXT-INDENT: 2em; FONT-FAMILY: 宋体; MARGIN-BOTTOM: 0px; FONT-SIZE: 12pt">为了巩固良好的航班正常工作态势，管制中心组织流量管理室和区域管制中心分别就7月27日，7月29日军航活动的协调、流量管理措施的发布以及对航班正常性的影响等进行了复盘，通过两次同样军航活动的处置对比，汲取工作经验并研究改进措施。</p><p style="TEXT-JUSTIFY: inter-ideograph; TEXT-ALIGN: justify; LINE-HEIGHT: 1.5; MARGIN-TOP: 0px; TEXT-INDENT: 2em; FONT-FAMILY: 宋体; MARGIN-BOTTOM: 0px; FONT-SIZE: 12pt"><br></p><p style="TEXT-JUSTIFY: inter-ideograph; TEXT-ALIGN: justify; LINE-HEIGHT: 1.5; MARGIN-TOP: 0px; TEXT-INDENT: 2em; FONT-FAMILY: 宋体; MARGIN-BOTTOM: 0px; FONT-SIZE: 12pt">通过此次复盘会，管制中心主动就改进航班正常性工作提出意见：一是一线运行部门在提出流量管理措施的建议时，一定要有较为充分的数据支持；二是流量管理部门对运行单位提出的流控建议要科学评估，该组织流量管理会商的一定要组织会商；三是遇有军航活动要充分协调，明确活动性质、避让方案的水平和垂直范围；四是尽可能争取可用空域，以便采取雷达引导方案；五是各运行单位接到军航运输机计划，前期一定要充分评估对本单位的影响，必要时向流量管理室申请会商，共同研究措施；六是发布流量管理措施要要素齐全、用语标准、含义明确；七是如果无法制定有效措施以避免对正常性的影响，应及时上报上级单位；八是要做好信息通报工作。</p><p style="TEXT-JUSTIFY: inter-ideograph; TEXT-ALIGN: justify; LINE-HEIGHT: 1.5; MARGIN-TOP: 0px; TEXT-INDENT: 2em; FONT-FAMILY: 宋体; MARGIN-BOTTOM: 0px; FONT-SIZE: 12pt"><br></p><p style="TEXT-JUSTIFY: inter-ideograph; TEXT-ALIGN: justify; LINE-HEIGHT: 1.5; MARGIN-TOP: 0px; TEXT-INDENT: 2em; FONT-FAMILY: 宋体; MARGIN-BOTTOM: 0px; FONT-SIZE: 12pt">通过开展典型航班正常处置工作复盘会议，管制中心强化了各级人员遵守规章制度的责任意识，在如何保障和提高航班正常性工作方面达成了共识，促进各方履责尽责，为管制中心进一步做好暑运期间航班正常性保障工作奠定了良好的基础。</p><p style="TEXT-JUSTIFY: inter-ideograph; TEXT-ALIGN: justify; LINE-HEIGHT: 1.5; MARGIN-TOP: 0px; TEXT-INDENT: 2em; FONT-FAMILY: 宋体; MARGIN-BOTTOM: 0px; FONT-SIZE: 12pt"><br></p></span>\n' +
-            '                                        \n' +
-            '                                    </td>\n' +
-            '                                </tr>\n' +
-            '                            </table>\n' +
-            '                        </td>\n' +
-            '                    </tr>\n' +
-            '                </table>\n' +
-            '            </td>\n' +
-            '    </table>\n'
-          }
+          })
+
+          console.log(this.$route.query.id)
 
         }
       }
@@ -103,4 +134,193 @@
 
 <style lang="less" scoped>
   @import '../../../../assets/styles/list_details.less';
+  .ewm {
+    width: 250px;
+    height: 250px;
+    display: block;
+    margin: 0 auto;
+    margin-top: 30px;
+  }
+</style>
+<style lang="less" scoped>
+  @import '../../../../assets/styles/list.less';
+  .title_hr{
+    background-color: #f8f8f8;
+    height: 10px;
+    width: 100%;
+  }
+  .banner_hr{
+    width: 1200px;
+    position: inherit;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    height: inherit;
+  }
+  .bar_hr{
+    background-color: #026ab3;
+    height: 100%;
+    width: 27%;
+  }
+  .detail-table {
+    .info {
+      width:100%;
+      margin-bottom: 52px;
+      background: #fff;
+      height:320px;
+      .img {
+        float: left;
+        width:574px;
+        height:320px;
+      }
+      .detail {
+        width:626px;
+        text-align: left;
+        float: left;
+        padding:0 25px;
+        box-sizing: border-box;
+        height:320px;
+        .title {
+          height:116px;
+          font-size: 26px;
+          color:#026ab3;
+          font-weight: 600;
+          display: table-cell;
+          vertical-align:middle
+        }
+        .time {
+          font-size: 16px;
+          color:#666666;
+          font-weight: normal;
+          line-height: 42px;
+        }
+        .icon {
+          color:#026ab3;
+          font-size: 20px;
+          margin-right: 20px;
+        }
+        .address {
+          font-size: 16px;
+          color:#666666;
+          line-height: 42px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+
+        }
+        .btn {
+          width:136px;
+          height:46px;
+          line-height: 46px;
+          color:#fff;
+          margin-top: 28px;
+          font-size: 18px;
+          cursor: pointer;
+          background: #da422a;
+          font-weight: 600;
+          text-align: center;
+        }
+        .wechart {
+          width:110px;
+          text-align: center;
+          float: right;
+          .img {
+            width:74px;
+            height:74px;
+            margin:0 auto;
+            display: block;
+            margin-bottom: 10px;
+          }
+          .name {
+            font-size: 14px;
+            color:#000;
+          }
+        }
+      }
+    }
+    .nav-tabs {
+      width: 100%;
+      height:86px;
+      margin-bottom: 20px;
+      background: #fff;
+      line-height: 86px;
+      text-align: center;
+      .tab {
+        width:224px;
+        cursor: pointer;
+        color:#000;
+        font-size: 24px;
+        float: left;
+        &.active {
+          color:#026ab3;
+          font-weight: 600;
+          position:relative;
+          &:after {
+            content: '';
+            display: block;
+            width:100%;
+            position: absolute;
+            bottom:0;
+            background: #026ab3;
+            left:0;
+            height:2px;
+          }
+        }
+      }
+    }
+    .nav-content {
+      .tab {
+        width:100%;
+        background: #fff;
+        text-align: left;
+        padding:14px 30px;
+        box-sizing: border-box;
+        margin-bottom: 20px;
+        .intro {
+          font-size: 18px;
+          color:#666666;
+        }
+        .title {
+          padding-left: 36px;
+          box-sizing: border-box;
+          position: relative;
+          margin-bottom: 10px;
+          font-size: 24px;
+          font-weight: 600;
+          &:before {
+            content: '';
+            display: block;
+            position: absolute;
+            width:3px;
+            top:50%;
+            transform: translateY(-50%);
+            -webkit-transform: translateY(-50%);
+            background: #026ab3;
+            left:0;
+            height:24px;
+          }
+        }
+        ul {
+          padding-left: 16px;
+          box-sizing: border-box;
+          list-style: disc;
+          li {
+            line-height: 34px;
+            font-size: 18px;
+            color:#666;
+          }
+        }
+        .upload {
+          font-size: 18px;
+          color:#da422a;
+          cursor: pointer;
+          text-decoration: underline;
+          font-weight: 600;
+          line-height: 60px;
+        }
+      }
+    }
+  }
 </style>
